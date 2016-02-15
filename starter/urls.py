4c1188ef8d29d3b87270604513bfc9e9aa4a1933
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-15 14:38:46 +0100

    removing unused from urls

--- starter/urls.py
+++ starter/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import url, include
 import rest_framework_jwt.views
@@ -28,7 +27,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', rest_framework_jwt.views.obtain_jwt_token),
 ]
