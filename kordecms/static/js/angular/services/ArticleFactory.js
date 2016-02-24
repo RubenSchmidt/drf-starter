3fd98aa499e4f401d654d1492c49991268f6d597
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-02-24 14:40:41 +0100

    restructured angular project, minified everything

---
+++ kordecms/static/js/angular/services/ArticleFactory.js
@@ -0,0 +1,38 @@
+/**
+ * Created by rubenschmidt on 24.02.2016.
+ */
+kordeCms.factory('ArticleFactory',
+    ['$http', 'apiUrl', function ($http, apiUrl) {
+        var endpoint = apiUrl + '/articles';
+        return ({
+            get: get,
+            list: list,
+            create: create,
+            update: update,
+            destroy: destroy,
+            count: count
+        });
+        function get(id) {
+            return $http.get(endpoint + '/' + id)
+        }
+
+        function list() {
+            return $http.get(endpoint)
+        }
+
+        function create(article) {
+            return $http.post(endpoint, article)
+        }
+
+        function update(article) {
+            return $http.put(endpoint + '/' + article.id, article)
+        }
+
+        function destroy(id) {
+            return $http.delete(endpoint + '/' + id)
+        }
+
+        function count() {
+            return $http.get(endpoint + '/count')
+        }
+    }]);
