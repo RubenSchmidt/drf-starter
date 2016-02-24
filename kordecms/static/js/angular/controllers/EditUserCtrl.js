a10eabf3f23b10a5fcfe9208c498a370c5f544e7
ocwendel <ole.wendel@gmail.com>
2016-02-24 17:35:59 +0100

    adjusted edit priviliges for users and admin

--- kordecms/static/js/angular/controllers/EditUserCtrl.js
+++ kordecms/static/js/angular/controllers/EditUserCtrl.js
@@ -18,16 +18,27 @@ kordeCms.controller('EditUserCtrl',
             //Error
         });
 
-
-
+        $scope.getRole = function (user) {
+            if (user.is_superuser) {
+                return "Admin";
+            }
+            else {
+                return "Bruker";
+            }
+        }
 
         $scope.canEditUser = function(user){
             return (user.id == $scope.currentUser.id || $scope.currentUser.is_superuser)
+        }
+        $scope.canChangeRole = function(user){
+            return ($scope.currentUser.is_superuser && $scope.currentUser.id != user.id);
         }
 
         var updateUser = function () {
             return (user.id != $scope.currentUser.id && $scope.currentUser.is_superuser)
         }
+
+
 
 
         $scope.updateUser = function() {
@@ -51,42 +62,42 @@ kordeCms.controller('EditUserCtrl',
 
 
 
-            $scope.password = {old_password: "", new_password_1: "", new_password_2: ""}
+        $scope.password = {old_password: "", new_password_1: "", new_password_2: ""}
 
-            $scope.updatePassword = function () {
+        $scope.updatePassword = function () {
                 updatePassword()
             }
 
 
-            var updatePassword = function () {
-                if ($scope.password.new_password_1 == $scope.password.new_password_2) {
-                    //New passwords match
-                    $http.post('/api/api-token-auth/', {
-                        username: $scope.user.username,
-                        password: $scope.password.old_password
-                    }).then(function (response) {
-                        //Old password correct
-                        if (response.status === 200 && response.data.token) {
-                            $scope.user.password = $scope.password.new_password_2;
-                            //Set new password to given new password
-                            UserFactory.updatePassword($scope.user).then(function (response) {
-                                //Success - password updated
-                                $location.path('/users')
-                            }, function (response) {
-                                //error - could not set user password to given passord
-                                $scope.errors = response.data;
-                                console.log($scope.errors);
-                            });
-                        }
-                    }, function (response) {
-                        //Handle error
-                        $scope.errors = response.data;
-                        console.log(response.data);
-                    });
-                }
-                else {
-                    $scope.errors = {newPassword: ["Forskjellig passord oppgitt"]}
-                }
-            };
+        var updatePassword = function () {
+            if ($scope.password.new_password_1 == $scope.password.new_password_2) {
+                //New passwords match
+                $http.post('/api/api-token-auth/', {
+                    username: $scope.user.username,
+                    password: $scope.password.old_password
+                }).then(function (response) {
+                    //Old password correct
+                    if (response.status === 200 && response.data.token) {
+                        $scope.user.password = $scope.password.new_password_2;
+                        //Set new password to given new password
+                        UserFactory.updatePassword($scope.user).then(function (response) {
+                            //Success - password updated
+                            $location.path('/users')
+                        }, function (response) {
+                            //error - could not set user password to given passord
+                            $scope.errors = response.data;
+                            console.log($scope.errors);
+                        });
+                    }
+                }, function (response) {
+                    //Handle error
+                    $scope.errors = response.data;
+                    console.log(response.data);
+                });
+            }
+            else {
+                $scope.errors = {newPassword: ["Forskjellig passord oppgitt"]}
+            }
+        };
 
     }]);
